a918168d703512560dfb92d6d51f52e066862929
sateda <7441698+sateda@users.noreply.github.com>
2025-03-09 14:42:31 +0100

    Completed rudimentary sketchpad

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("test");
+// Create the pixels (DIVS) for the sketchpad
+const sketchpad = document.querySelector("#sketchpad");
+
+square = 960*960 / 16;
+width = Math.sqrt(square);
+console.log(width);
+
+for(let i=0; i < 16; i++) {
+    const pixelDiv = document.createElement("div");
+    pixelDiv.classList.add("pixel");
+    pixelDiv.style = "min-width: " + width + "px;" + "height: " + width + "px;";
+    sketchpad.appendChild(pixelDiv);
+};
+
+// Listener mouseover event
+const pixels = document.querySelectorAll('.pixel');
+
+// Loop through all the pixel and add mouseover event listener
+pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+        pixel.classList.add("pixel-mouseover");
+    });
+});
+
