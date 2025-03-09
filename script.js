d3fd44ea1322a8acaa336ca2a09bc71ddded5b91
sateda <7441698+sateda@users.noreply.github.com>
2025-03-09 19:44:29 +0100

    Complete feature adjust grid size

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 // Create the pixels (DIVS) for the sketchpad
 const sketchpad = document.querySelector("#sketchpad");
 
-square = 960*960 / 16;
-width = Math.sqrt(square);
-console.log(width);
+// Draw the sketchpad
+function drawSketchpad(gridSize) { 
+    // Determine the size of a DIV based on the amount of pixels on a side
+    let pixelTotals = gridSize * gridSize;  
+    let pixelLength = 960 / gridSize;
 
-for(let i=0; i < 16; i++) {
-    const pixelDiv = document.createElement("div");
-    pixelDiv.classList.add("pixel");
-    pixelDiv.style = "min-width: " + width + "px;" + "height: " + width + "px;";
-    sketchpad.appendChild(pixelDiv);
+    for(let i=0; i < pixelTotals; i++) {
+        const pixelDiv = document.createElement("div");
+        pixelDiv.classList.add("pixel");
+        pixelDiv.style = "min-width: " + pixelLength + "px;" + "height: " + pixelLength + "px;";
+        sketchpad.appendChild(pixelDiv);
+    };
+
+    addListenersToPixels();
 };
 
-// Listener mouseover event
-const pixels = document.querySelectorAll('.pixel');
+function addListenersToPixels() {
+    // Listener mouseover event
+    const pixels = document.querySelectorAll(".pixel");
 
-// Loop through all the pixel and add mouseover event listener
-pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", () => {
-        pixel.classList.add("pixel-mouseover");
+    // Loop through all the pixel and add mouseover event listener
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseover", () => {
+            pixel.classList.add("pixel-mouseover");
+        });
     });
+};
+
+// function to remove all Pixel DIVS
+function removePixels() {
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach((pixel) => {
+        sketchpad.removeChild(pixel);
+    });
+};
+
+// Button grid size event listener
+const buttonGridSize = document.querySelector("#buttonGrid");
+buttonGridSize.addEventListener("click", () => {
+    gridSize = prompt("Please enter you're desired grid length");
+    removePixels();
+    drawSketchpad(gridSize);
 });
 
+// initialize sketchpad
+let defaultGrid = 4;
+drawSketchpad(defaultGrid);
